7e38e20a8e88f6486a99b6845a75f925856d50e2
Phirapaht Kitpromphon <63070125@kmitl.ac.th>
2023-04-03 20:57:12 +0700

    commit c

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -82,7 +82,7 @@ function App() {
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit" onClick={() => submit()}>
+        <Button variant="primary" onClick={() => submit()}>
           Submit
         </Button>
       </Form>
